82e5f764dc6a21242da8c40cf8034e772bf8c65d
linhvo4712 <71310749+linhvo4712@users.noreply.github.com>
2020-10-05 20:11:25 +0700

    Add files via upload

---
+++ tut3SS1/BubbleSort.py
@@ -0,0 +1,24 @@
+#Python program to implement bubble sort
+def bubblesort(arr):
+    n = len(arr)
+    #Traverse through all array's elements
+    for i in range (n-1):
+        # range(n) also work but outer loop will repeat one time more than needed.
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+        # traverse the array from 0 to n-i-1
+        # Swap if the element found is greater
+        # than the next element
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+#test case
+def main():
+    arr = [64, 34, 25, 12, 22, 11, 90]
+
+    bubbleSort(arr)
+
+    print("Sorted array is:")
+    for i in range(len(arr)):
+        print("%d" % arr[i])
